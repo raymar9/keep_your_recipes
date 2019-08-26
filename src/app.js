3fbbf2821c333b480f7906600e7990ed10031839
raymar9 <raymar@mailbox.org>
2019-08-26 22:51:12 +0200

    create get and post rest call routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const recipeManager = require('./model/recipe_manager');
 
 const app = express();
 
 app.set('x-powered-by', false);
 
-// static content
-app.use(express.static('public'));
+// Routing
 
+// provide static content
+app.use(express.static('public'));
+// for parsing application/json
+app.use(express.json());
+
+// REST routing
+// GET all recipes
+app.route('/recipes')
+    .get((req, res, next) => {
+        console.debug('GET request: get all recipes');
+        recipeManager.getAllRecipesFromDatabase()
+            .then((result) => res.json(result))
+            .catch(next);
+    })
+
+    .post((req, res, next) => {
+        console.debug('POST request: add new recipe');
+        let rB = req.body;
+        let newRecipe = new recipeManager.Recipe(rB.title, rB.complexity, rB.tags, rB.ingredients, rB. preparation, rB.image);
+        recipeManager.addRecipeToDatabase(newRecipe)
+            .then(() => res.sendStatus(201))
+            .catch(next);
+    })
+
+// Start backend application by listening on port...
 app.listen(8081);
