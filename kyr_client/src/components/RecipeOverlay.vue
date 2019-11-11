17154f6ae411a686d38df4c21b6e206fcd87abd1
raymar9 <raymar@mailbox.org>
2019-11-11 22:11:16 +0100

    finish edit mode in overlay state

--- kyr_client/src/components/RecipeOverlay.vue
+++ kyr_client/src/components/RecipeOverlay.vue
@@ -15,10 +15,10 @@
         <div class="tags are-medium">
           <span class="tag is-rounded" v-for="tag in recipe.tags" :key="tag.id">{{ tag }}</span>
         </div>
-        <ul>
+        <ul class="ingredients">
           <li v-for="ingredient in recipe.ingredients" :key="ingredient.id">{{ ingredient }}</li>
         </ul>
-        <p>{{ recipe.preparation }}</p>
+        <p class="preparation">{{ recipe.preparation }}</p>
       </section>
       <footer class="modal-card-foot"></footer>
     </div>
@@ -30,23 +30,30 @@
       </header>
       <section class="modal-card-body">
         <div class="tags are-medium">
-          <span class="tag is-rounded" v-for="tag in modifiedRecipe.tags" :key="tag.id">{{ tag }}
-            <span class="icon is-small"><i class="oi oi-x"></i></span>
+          <span class="tag is-rounded" v-for="(tag, index) in modifiedRecipe.tags" :key="tag.id">{{ tag }}
+            <span class="icon is-small" @click="removeTag(index)"><i class="oi oi-x"></i></span>
           </span>
-          <span class="tag is-rounded"><input v-model="newTag" placeholder="Add new tag..."><span class="icon is-small" @click="addTag"><i class="oi oi-plus"></i></span></span>
+          <span class="tag is-rounded"><input v-model="newTag" placeholder="Add new tag...">
+          <span class="icon is-small" @click="addTag"><i class="oi oi-plus"></i></span></span>
         </div>
-        <ul>
-          <li v-for="ingredient in modifiedRecipe.ingredients" :key="ingredient.id">{{ ingredient }}</li>
+        <ul class="ingredients">
+          <li class="ingredient" v-for="(ingredient, index) in modifiedRecipe.ingredients" :key="ingredient.id">
+            <span class="icon is-small" @click="removeIngredient(index)"><i class="oi oi-x"></i></span>
+            {{ ingredient }}
+          </li>
+          <li class="ingredient"><input v-model="newIngredient" placeholder="Add new ingredient...">
+          <span class="icon is-small" @click="addIngredient"><i class="oi oi-plus"></i></span></li>
         </ul>
-        <p>{{ modifiedRecipe.preparation }}</p>
+        <textarea class="preparation" v-model="modifiedRecipe.preparation"></textarea>
       </section>
-      <footer class="modal-card-foot"></footer>
+      <footer class="modal-card-foot"><button class="button" @click="saveChanges">Save</button></footer>
     </div>
 
   </div>
 </template>
 
 <script>
+import { checkIngredient, checkTag } from "../recipe_model/recipe_utils";
 export default {
   name: "RecipeOverlay",
   props: ["recipe"],
@@ -54,8 +61,9 @@ export default {
     return {
       showDeletionConfirmation: false,
       showEditMode: false,
-      modifiedRecipe: Object.assign({}, this.recipe),
-      newTag: undefined
+      modifiedRecipe: JSON.parse(JSON.stringify(this.recipe)),
+      newTag: undefined,
+      newIngredient: undefined
     }
   },
   methods: {
@@ -66,18 +74,50 @@ export default {
       this.showEditMode = state;
     },
     addTag: function() {
-      this.modifiedRecipe.tags.push(this.newTag);
+      if (checkTag(this.newTag)) {
+        this.modifiedRecipe.tags.push(this.newTag.trim());
+        this.newTag = undefined;
+      }
+    },
+    removeTag: function(tagId) {
+      console.log(tagId);
+      this.modifiedRecipe.tags.splice(tagId, 1);
+    },
+    addIngredient: function() {
+      if (checkIngredient(this.newIngredient)) {
+        this.modifiedRecipe.ingredients.push(this.newIngredient.trim());
+        this.newIngredient = undefined;
+      }
+    },
+    removeIngredient: function(ingredientId) {
+      this.modifiedRecipe.ingredients.splice(ingredientId, 1);
+    },
+    saveChanges: function() {
+      this.toggleEditMode(false);
+      this.$emit('save-recipe', this.modifiedRecipe);
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 header a.icon {
   margin-left: 1rem;
 }
 span.tag span.icon {
   padding-left: 1.5rem;
   padding-right: 1rem;
+}
+ul.ingredients {
+  margin-top: 1rem;
+}
+li.ingredient span.icon {
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
+}
+textarea.preparation, p.preparation{
+  margin-top: 1rem;
+  width: 100%;
+  height: 6rem;
 }
 </style>
