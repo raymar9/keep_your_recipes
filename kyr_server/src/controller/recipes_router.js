51ec3ddbfce1b8f91fe481e1464ca02a4abc9926
raymar9 <raymar@mailbox.org>
2020-02-17 23:42:18 +0100

    [server] extend cors header settings with wildcards for development purpose

--- kyr_server/src/controller/recipes_router.js
+++ kyr_server/src/controller/recipes_router.js
@@ -4,8 +4,9 @@ const recipeManager = require('../model/recipe_manager');
 const router = express.Router();
 
 router.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
